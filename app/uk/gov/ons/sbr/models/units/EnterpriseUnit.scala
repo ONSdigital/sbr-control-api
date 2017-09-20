98c6b52b3d5fb6c6502230035651cc0c9e0bee7f
ONS-Tom <Tom.Cooling@ons.gov.uk>
2017-09-20 11:32:48 +0100

    Add childrenJson to EnterpriseUnit model (#14)
    - New variable in the model, childrenJson, which returns a List[JsValue]

--- app/uk/gov/ons/sbr/models/units/EnterpriseUnit.scala
+++ app/uk/gov/ons/sbr/models/units/EnterpriseUnit.scala
@@ -1,8 +1,9 @@
 package uk.gov.ons.sbr.models.units
 
 import io.swagger.annotations.ApiModelProperty
-import play.api.libs.json.{ JsValue, Json, OFormat }
+//import play.api.libs.json.{ JsValue, Json, OFormat }
 import uk.gov.ons.sbr.data.domain.Enterprise
+import play.api.libs.json._
 
 import scala.collection.JavaConversions._
 /**
@@ -17,6 +18,7 @@ case class EnterpriseUnit(
   @ApiModelProperty(value = "A map of parents of returned id [Type, Value]", example = "",
     dataType = "Map[String,String]") parents: Map[String, String],
   @ApiModelProperty(value = "A string of all related children", example = "") children: Map[String, String],
+  @ApiModelProperty(value = "A string of all related children", example = "") childrenJson: List[JsValue],
   unitType: String
 )
 
@@ -25,11 +27,13 @@ object EnterpriseUnit {
   implicit val unitFormat: OFormat[EnterpriseUnit] = Json.format[EnterpriseUnit]
 
   def apply(o: Enterprise): EnterpriseUnit = {
-    //o.getChildren
     val childMap = o.getLinks.getChildren.map(
       z => (z._1, z._2.toString)
     ).toMap
-    EnterpriseUnit(o.getKey.toLong, o.getReferencePeriod.toString, o.getVariables.toMap, Map(), childMap, o.getType.toString)
+    val childJson = o.getChildren.map {
+      a => Json.parse(a.toUnitHierarchyAsJson)
+    }.toList
+    EnterpriseUnit(o.getKey.toLong, o.getReferencePeriod.toString, o.getVariables.toMap, Map(), childMap, childJson, o.getType.toString)
   }
 
   def toJson(o: Enterprise): JsValue = Json.toJson(apply(o))
