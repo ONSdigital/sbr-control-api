afebb35c647a6e8e3b13a0daae929dcb77ee8ecc
urquha <andrew.urquhart@ons.gov.uk>
2018-02-15 12:23:56 +0000

    Updated with companies, paye, vat and urbn. All tests passed.

--- test/server/HBaseRestTests.scala
+++ test/server/HBaseRestTests.scala
@@ -70,19 +70,55 @@ class HBaseRestTests extends TestUtils with BeforeAndAfterEach with GuiceOneAppP
     }
 
     "return a unit for a valid id (company)" in {
-      1 must equal(1)
+      val id = "01752564"
+      val body = "{\"Row\":[{\"key\":\"MjAxNzA2fjAxNzUyNTY0fkNI\",\"Cell\":[{\"column\":\"ZDpwX0VOVA==\",\"timestamp\":1518617008390,\"$\":\"OTkwMDE1NjExNQ==\"},{\"column\":\"ZDpwX0xFVQ==\",\"timestamp\":1518617010555,\"$\":\"MTAyMDU0MTU=\"}]}]}"
+      mockEndpoint(unitLinksTable, firstPeriod, id, "*", body)
+      val resp = fakeRequest(s"/$version/units/$id")
+      val json = contentAsJson(resp).as[JsArray]
+      val unit = json(0).validate[UnitLinks]
+      status(resp) mustBe OK
+      contentType(resp) mustBe Some("application/json")
+      json.value.size mustBe 1
+      unit.isInstanceOf[JsSuccess[UnitLinks]] mustBe true
     }
 
     "return a unit for a valid id (VAT)" in {
-      1 must equal(1)
+      val id = "401347263289"
+      val body = "{\"Row\":[{\"key\":\"MjAxNzA2fjQwMTM0NzI2MzI4OX5WQVQ=\",\"Cell\":[{\"column\":\"ZDpwX0VOVA==\",\"timestamp\":1518617001775,\"$\":\"OTkwMDE1NjExNQ==\"},{\"column\":\"ZDpwX0xFVQ==\",\"timestamp\":1518617016005,\"$\":\"MTAyMDU0MTU=\"}]}]}"
+      mockEndpoint(unitLinksTable, firstPeriod, id, "*", body)
+      val resp = fakeRequest(s"/$version/units/$id")
+      val json = contentAsJson(resp).as[JsArray]
+      val unit = json(0).validate[UnitLinks]
+      status(resp) mustBe OK
+      contentType(resp) mustBe Some("application/json")
+      json.value.size mustBe 1
+      unit.isInstanceOf[JsSuccess[UnitLinks]] mustBe true
     }
 
     "return a unit for a valid id (PAYE)" in {
-      1 must equal(1)
+      val id = "2738768"
+      val body = "{\"Row\":[{\"key\":\"MjAxNzA2fjI3Mzg3Njh+UEFZRQ==\",\"Cell\":[{\"column\":\"ZDpwX0VOVA==\",\"timestamp\":1518617005123,\"$\":\"OTkwMDE1NjExNQ==\"},{\"column\":\"ZDpwX0xFVQ==\",\"timestamp\":1518617012781,\"$\":\"MTAyMDU0MTU=\"}]}]}"
+      mockEndpoint(unitLinksTable, firstPeriod, id, "*", body)
+      val resp = fakeRequest(s"/$version/units/$id")
+      val json = contentAsJson(resp).as[JsArray]
+      val unit = json(0).validate[UnitLinks]
+      status(resp) mustBe OK
+      contentType(resp) mustBe Some("application/json")
+      json.value.size mustBe 1
+      unit.isInstanceOf[JsSuccess[UnitLinks]] mustBe true
     }
 
     "return a unit for a valid id (UBRN)" in {
-      1 must equal(1)
+      val id = "10205415"
+      val body = "{\"Row\":[{\"key\":\"MjAxNzA2fjEwMjA1NDE1fkxFVQ==\",\"Cell\":[{\"column\":\"ZDpjXzAxNzUyNTY0\",\"timestamp\":1518617010555,\"$\":\"Q0g=\"},{\"column\":\"ZDpjXzI3Mzg3Njg=\",\"timestamp\":1518617012781,\"$\":\"UEFZRQ==\"},{\"column\":\"ZDpjXzQwMTM0NzI2MzI4OQ==\",\"timestamp\":1518617016005,\"$\":\"VkFU\"},{\"column\":\"ZDpwX0VOVA==\",\"timestamp\":1518616999611,\"$\":\"OTkwMDE1NjExNQ==\"}]}]}"
+      mockEndpoint(unitLinksTable, firstPeriod, id, "*", body)
+      val resp = fakeRequest(s"/$version/units/$id")
+      val json = contentAsJson(resp).as[JsArray]
+      val unit = json(0).validate[UnitLinks]
+      status(resp) mustBe OK
+      contentType(resp) mustBe Some("application/json")
+      json.value.size mustBe 1
+      unit.isInstanceOf[JsSuccess[UnitLinks]] mustBe true
     }
 
     // This test should pass but currently doesn't
@@ -106,22 +142,90 @@ class HBaseRestTests extends TestUtils with BeforeAndAfterEach with GuiceOneAppP
   }
 
   "/v1/periods/:period/units/:unit" should {
+    "return a unit for a valid id (company)" in {
+      val id = "01752564"
+      val body = "{\"Row\":[{\"key\":\"MjAxNzA2fjAxNzUyNTY0fkNI\",\"Cell\":[{\"column\":\"ZDpwX0VOVA==\",\"timestamp\":1518617008390,\"$\":\"OTkwMDE1NjExNQ==\"},{\"column\":\"ZDpwX0xFVQ==\",\"timestamp\":1518617010555,\"$\":\"MTAyMDU0MTU=\"}]}]}"
+      mockEndpoint(unitLinksTable, firstPeriod, id, "*", body)
+      val resp = fakeRequest(s"/$version/units/$id")
+      val json = contentAsJson(resp).as[JsArray]
+      val unit = json(0).validate[UnitLinks]
+      status(resp) mustBe OK
+      contentType(resp) mustBe Some("application/json")
+      json.value.size mustBe 1
+      unit.isInstanceOf[JsSuccess[UnitLinks]] mustBe true
+    }
 
   }
-
   "/v1/enterprises/:id" should {
-
+    "return a unit for a valid id (enterprise)" in {
+      val id = "9905899133"
+      val body = "{\"Row\":[{\"key\":\"MjAxNzA2fjk5MDU4OTkxMzN+RU5U\",\"Cell\":[{\"column\":\"ZDpjXzA0MTA5NDM2\",\"timestamp\":1518617008390,\"$\":\"Q0g=\"},{\"column\":\"ZDpjXzA1ODQ0NTE2\",\"timestamp\":1518617008390,\"$\":\"Q0g=\"},{\"column\":\"ZDpjXzE0MDkyOTE5\",\"timestamp\":1518616999611,\"$\":\"TEVV\"},{\"column\":\"ZDpjXzIzOTE0OTg=\",\"timestamp\":1518617005123,\"$\":\"UEFZRQ==\"},{\"column\":\"ZDpjXzI2ODAyMzcx\",\"timestamp\":1518616999611,\"$\":\"TEVV\"},{\"column\":\"ZDpjXzM4ODM2NTI=\",\"timestamp\":1518617005123,\"$\":\"UEFZRQ==\"},{\"column\":\"ZDpjXzU2NzM4Mzk1\",\"timestamp\":1518616999611,\"$\":\"TEVV\"},{\"column\":\"ZDpjXzYwNjE2NDEzNDY0MA==\",\"timestamp\":1518617001775,\"$\":\"VkFU\"},{\"column\":\"ZDpjXzYyMTM0NzY=\",\"timestamp\":1518617005123,\"$\":\"UEFZRQ==\"},{\"column\":\"ZDpjXzY0MjA2NjMyMzg2NQ==\",\"timestamp\":1518617001775,\"$\":\"VkFU\"},{\"column\":\"ZDpjXzg4NjQxMTQ4MjMzNQ==\",\"timestamp\":1518617001775,\"$\":\"VkFU\"},{\"column\":\"ZDpjXzk3NjM1MTgzNjI5MQ==\",\"timestamp\":1518617001775,\"$\":\"VkFU\"},{\"column\":\"ZDpjX05JMDM0MTU5\",\"timestamp\":1518617008390,\"$\":\"Q0g=\"}]}]}"
+      mockEndpoint(unitLinksTable, firstPeriod, id, "*", body)
+      val resp = fakeRequest(s"/$version/units/$id")
+      val json = contentAsJson(resp).as[JsArray]
+      val unit = json(0).validate[UnitLinks]
+      status(resp) mustBe OK
+      contentType(resp) mustBe Some("application/json")
+      json.value.size mustBe 1
+      unit.isInstanceOf[JsSuccess[UnitLinks]] mustBe true
+    }
   }
 
   "/v1/periods/:period/enterprises/:id" should {
-
+    "return a unit for a valid id (UBRN)" in {
+      val id = "10205415"
+      val body = "{\"Row\":[{\"key\":\"MjAxNzA2fjEwMjA1NDE1fkxFVQ==\",\"Cell\":[{\"column\":\"ZDpjXzAxNzUyNTY0\",\"timestamp\":1518617010555,\"$\":\"Q0g=\"},{\"column\":\"ZDpjXzI3Mzg3Njg=\",\"timestamp\":1518617012781,\"$\":\"UEFZRQ==\"},{\"column\":\"ZDpjXzQwMTM0NzI2MzI4OQ==\",\"timestamp\":1518617016005,\"$\":\"VkFU\"},{\"column\":\"ZDpwX0VOVA==\",\"timestamp\":1518616999611,\"$\":\"OTkwMDE1NjExNQ==\"}]}]}"
+      mockEndpoint(unitLinksTable, firstPeriod, id, "*", body)
+      val resp = fakeRequest(s"/$version/units/$id")
+      val json = contentAsJson(resp).as[JsArray]
+      val unit = json(0).validate[UnitLinks]
+      status(resp) mustBe OK
+      contentType(resp) mustBe Some("application/json")
+      json.value.size mustBe 1
+      unit.isInstanceOf[JsSuccess[UnitLinks]] mustBe true
+    }
   }
 
   "/v1/types/:type/units/:id" should {
-
+    "return a unit for a valid id (VatRef)" in {
+      val id = "401347263289"
+      val body = "{\"Row\":[{\"key\":\"MjAxNzA2fjQwMTM0NzI2MzI4OX5WQVQ=\",\"Cell\":[{\"column\":\"ZDpwX0VOVA==\",\"timestamp\":1518617001775,\"$\":\"OTkwMDE1NjExNQ==\"},{\"column\":\"ZDpwX0xFVQ==\",\"timestamp\":1518617016005,\"$\":\"MTAyMDU0MTU=\"}]}]}"
+      mockEndpoint(unitLinksTable, firstPeriod, id, "*", body)
+      val resp = fakeRequest(s"/$version/units/$id")
+      val json = contentAsJson(resp).as[JsArray]
+      val unit = json(0).validate[UnitLinks]
+      status(resp) mustBe OK
+      contentType(resp) mustBe Some("application/json")
+      json.value.size mustBe 1
+      unit.isInstanceOf[JsSuccess[UnitLinks]] mustBe true
+    }
   }
 
   "/v1/periods/:period/types/:type/units/:id" should {
-
+    "return a unit for a valid id (PayeRef)" in {
+      val id = "2738768"
+      val body = "{\"Row\":[{\"key\":\"MjAxNzA2fjI3Mzg3Njh+UEFZRQ==\",\"Cell\":[{\"column\":\"ZDpwX0VOVA==\",\"timestamp\":1518617005123,\"$\":\"OTkwMDE1NjExNQ==\"},{\"column\":\"ZDpwX0xFVQ==\",\"timestamp\":1518617012781,\"$\":\"MTAyMDU0MTU=\"}]}]}"
+      mockEndpoint(unitLinksTable, firstPeriod, id, "*", body)
+      val resp = fakeRequest(s"/$version/units/$id")
+      val json = contentAsJson(resp).as[JsArray]
+      val unit = json(0).validate[UnitLinks]
+      status(resp) mustBe OK
+      contentType(resp) mustBe Some("application/json")
+      json.value.size mustBe 1
+      unit.isInstanceOf[JsSuccess[UnitLinks]] mustBe true
+    }
   }
+
+  //  "return a unit for a valid id ()" in {
+  //    val id = ""
+  //    val body = ""
+  //    mockEndpoint(unitLinksTable, firstPeriod, id, "*", body)
+  //    val resp = fakeRequest(s"/$version/units/$id")
+  //    val json = contentAsJson(resp).as[JsArray]
+  //    val unit = json(0).validate[UnitLinks]
+  //    status(resp) mustBe OK
+  //    contentType(resp) mustBe Some("application/json")
+  //    json.value.size mustBe 1
+  //    unit.isInstanceOf[JsSuccess[UnitLinks]] mustBe true
+  //  }
 }
