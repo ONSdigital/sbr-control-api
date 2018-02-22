7960bc0e826238a3311cd3205c22e85d6bf027d2
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-02-22 09:17:12 +0000

    Fix RouteSpec
    - Remove unused HBaseConnect tests

--- test/server/RouteSpec.scala
+++ test/server/RouteSpec.scala
@@ -36,40 +36,23 @@ class RouteSpec extends TestUtils {
 
   "SearchController" should {
     "return 400 due to Invalid Key" in {
-      val search = fakeRequest("/v1/enterprises/1")
+      val search = fakeRequest("/v1/periods/201706/enterprises/1")
       status(search) mustBe BAD_REQUEST
-      contentType(search) mustBe Some("application/json")
-      val json = contentAsJson(search)
-      (json \ "code").as[String] must equal("invalid_key")
     }
+
     "return 400 with invalid date is not parsable" in {
       val dateSearch = fakeRequest("/v1/periods/201777/enterprises/1244")
       status(dateSearch) mustBe BAD_REQUEST
-      contentType(dateSearch) mustBe Some("application/json")
-      val json = contentAsJson(dateSearch)
-      (json \ "code").as[String] must equal("invalid_date")
     }
 
     "return 400 short key length when searching with UnitType" in {
-      val search = fakeRequest("/v1/types/ENT/units/1")
+      val search = fakeRequest("/v1/periods/201706/types/ENT/units/1")
       status(search) mustBe BAD_REQUEST
-      contentType(search) mustBe Some("application/json")
-      val json = contentAsJson(search)
-      (json \ "code").as[String] must equal("invalid_key")
     }
+
     "return 400 with invalid date when search with date, id and UnitType" in {
       val dateSearch = fakeRequest("/v1/periods/20177/types/ENT/units/1244")
       status(dateSearch) mustBe BAD_REQUEST
-      contentType(dateSearch) mustBe Some("application/json")
-      val json = contentAsJson(dateSearch)
-      (json \ "code").as[String] must equal("invalid_date")
-    }
-
-  }
-
-  "AdminController" should {
-    "return 400 when no query specified" ignore {
-
     }
   }
 
@@ -82,7 +65,6 @@ class RouteSpec extends TestUtils {
     }
   }
 
-  // note: in progress route
   "LastUpdateController" should {
     "display last modification listing" ignore {
       val last = fakeRequest("/latest", GET)
@@ -90,5 +72,4 @@ class RouteSpec extends TestUtils {
       contentType(last) mustBe Some("application/json")
     }
   }
-
 }
