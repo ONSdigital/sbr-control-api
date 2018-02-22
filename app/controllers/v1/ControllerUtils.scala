4154b0bf76583374b3b4e878065fb93f59f67e66
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-02-22 10:56:03 +0000

    Remove unused files

--- app/controllers/v1/ControllerUtils.scala
+++ app/controllers/v1/ControllerUtils.scala
@@ -2,23 +2,16 @@ package controllers.v1
 
 import java.time.YearMonth
 import java.time.format.{ DateTimeFormatter, DateTimeParseException }
-import java.util.Optional
-import javax.naming.ServiceUnavailableException
 
-import scala.collection.JavaConversions._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{ Future, TimeoutException }
 import scala.util.{ Failure, Success, Try }
 
 import com.typesafe.scalalogging.StrictLogging
 import play.api.libs.json._
-import play.api.mvc.{ AnyContent, Controller, Request, Result }
+import play.api.mvc.{ AnyContent, Controller, Request }
 
 import uk.gov.ons.sbr.models.EditEnterprise
-import uk.gov.ons.sbr.models.units.{ EnterpriseUnit, KnownUnitLinks, UnitLinks }
 
 import config.Properties.minKeyLength
-import utils.Utilities.errAsJson
 import utils._
 
 /**
@@ -38,23 +31,6 @@ trait ControllerUtils extends Controller with StrictLogging {
         logger.error("cannot parse date to YearMonth object", ex)
         InvalidReferencePeriod(key, ex)
     }
-  }
-
-  protected[this] def tryAsResponse(parseToJson: Try[JsValue]): Result = parseToJson match {
-    case Success(s) => Ok(s)
-    case Failure(ex) =>
-      logger.error("Failed to parse instance to expected json format", ex)
-      BadRequest(errAsJson(BAD_REQUEST, "bad_request", s"Could not perform action with exception $ex"))
-  }
-
-  protected def matchByParams(id: Option[String], request: Request[AnyContent], date: Option[String] = None): RequestEvaluation = {
-    val key = id.orElse(request.getQueryString("id")).getOrElse("")
-    if (key.length >= minKeyLength) {
-      date match {
-        case None => IdRequest(key)
-        case Some(s) => validateYearMonth(key, s)
-      }
-    } else { InvalidKey(key) }
   }
 
   protected def matchByEditParams(id: Option[String], request: Request[AnyContent], period: Option[String] = None): RequestEvaluation = {
@@ -82,21 +58,4 @@ trait ControllerUtils extends Controller with StrictLogging {
       case u: JsError => InvalidEditJson(key, u)
     }
   }
-
-  protected def toOption[X](o: Optional[X]) = if (o.isPresent) Some(o.get) else None
-
-  protected def toJavaOptional[A](o: Option[A]): Optional[A] =
-    o match { case Some(a) => Optional.ofNullable(a); case _ => Optional.empty[A] }
-
-  protected def responseException: PartialFunction[Throwable, Result] = {
-    case ex: DateTimeParseException =>
-      BadRequest(errAsJson(BAD_REQUEST, "invalid_date", s"cannot parse date exception found $ex"))
-    case ex: RuntimeException => InternalServerError(errAsJson(INTERNAL_SERVER_ERROR, "runtime_exception", s"$ex"))
-    case ex: ServiceUnavailableException =>
-      ServiceUnavailable(errAsJson(SERVICE_UNAVAILABLE, "service_unavailable", s"$ex"))
-    case ex: TimeoutException =>
-      RequestTimeout(errAsJson(REQUEST_TIMEOUT, "request_timeout", s"This may be due to connection being blocked. $ex"))
-    case ex => InternalServerError(errAsJson(INTERNAL_SERVER_ERROR, "internal_server_error", s"$ex."))
-  }
-
 }
