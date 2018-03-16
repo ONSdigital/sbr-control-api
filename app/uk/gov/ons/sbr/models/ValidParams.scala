1e7fa5cd16ba14811303d1a043d6d92bde8e9aa2
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-03-16 12:34:31 +0000

    Add test for ValidParams models
    - Change all Either uses to make Right the 'right' case

--- app/uk/gov/ons/sbr/models/ValidParams.scala
+++ app/uk/gov/ons/sbr/models/ValidParams.scala
@@ -10,27 +10,27 @@ sealed trait ValidParams
 
 case class UnitLinksParams(id: String) extends ValidParams
 object UnitLinksParams extends ValidParams {
-  def validate(id: String): Either[UnitLinksParams, InvalidParams] = id match {
-    case i if (!validId(i)) => Right(InvalidId())
-    case i => Left(UnitLinksParams(i))
+  def validate(id: String): Either[InvalidParams, UnitLinksParams] = id match {
+    case i if (!validId(i)) => Left(InvalidId())
+    case i => Right(UnitLinksParams(i))
   }
 }
 
 case class EnterpriseParams(id: String, period: Option[String]) extends ValidParams
 object EnterpriseParams extends ValidParams {
-  def validate(id: String, period: Option[String]): Either[EnterpriseParams, InvalidParams] = (id, period) match {
-    case (i, _) if (!validId(i)) => Right(InvalidId())
-    case (_, Some(p)) if (!validPeriod(p)) => Right(InvalidPeriod())
-    case (i, p) => Left(EnterpriseParams(i, p))
+  def validate(id: String, period: Option[String]): Either[InvalidParams, EnterpriseParams] = (id, period) match {
+    case (i, _) if (!validId(i)) => Left(InvalidId())
+    case (_, Some(p)) if (!validPeriod(p)) => Left(InvalidPeriod())
+    case (i, p) => Right(EnterpriseParams(i, p))
   }
 }
 
 case class StatUnitLinksParams(id: String, category: String, period: String) extends ValidParams
 object StatUnitLinksParams extends ValidParams {
-  def validate(id: String, period: String, category: String): Either[StatUnitLinksParams, InvalidParams] = (id, period, category) match {
-    case (i, _, _) if (!validId(i)) => Right(InvalidId())
-    case (_, p, _) if (!validPeriod(p)) => Right(InvalidPeriod())
-    case (_, _, c) if (!validCategory(c)) => Right(InvalidCategory())
-    case (i, p, c) => Left(StatUnitLinksParams(i, c, p))
+  def validate(id: String, category: String, period: String): Either[InvalidParams, StatUnitLinksParams] = (id, period, category) match {
+    case (i, _, _) if (!validId(i)) => Left(InvalidId())
+    case (_, p, _) if (!validPeriod(p)) => Left(InvalidPeriod())
+    case (_, _, c) if (!validCategory(c)) => Left(InvalidCategory())
+    case (i, p, c) => Right(StatUnitLinksParams(i, c, p))
   }
 }
