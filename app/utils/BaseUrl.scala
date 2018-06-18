429f30736ec19cd53a9391576cb2b75826a23a5f
urquha <andrew.urquhart@ons.gov.uk>
2018-06-18 16:42:49 +0100

    removed unused code

--- app/utils/BaseUrl.scala
+++ app/utils/BaseUrl.scala
@@ -3,10 +3,7 @@ package utils
 case class BaseUrl(protocol: String, host: String, port: Int, prefix: Option[String])
 
 object BaseUrl {
-  object Protocol {
-    val Http = "http"
-  }
-
+  
   def asUrlString(baseUrl: BaseUrl): String =
     s"${baseUrl.protocol}://${baseUrl.host}:${baseUrl.port}/${baseUrl.prefix.fold("")(p => if (p.trim.isEmpty()) "" else p + "/")}"
 }
