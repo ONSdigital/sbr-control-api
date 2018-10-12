ef8a2bd11d02b8664666080d659e1b1c1e4ed3e0
Nigel Perkins <nigelhp@yahoo.co.uk>
2018-10-12 15:10:14 +0100

    REG-1668: refactor JSON represenntation of Reporting Unit

--- it/fixture/ReadsReportingUnit.scala
+++ it/fixture/ReadsReportingUnit.scala
@@ -1,11 +1,14 @@
 package fixture
 
-import play.api.libs.json.{ Json, Reads }
-import uk.gov.ons.sbr.models.enterprise.Ern
-import uk.gov.ons.sbr.models.reportingunit.{ ReportingUnit, Rurn }
+import play.api.libs.json.{Json, Reads}
+import uk.gov.ons.sbr.models.Address
+import uk.gov.ons.sbr.models.enterprise.{EnterpriseLink, Ern}
+import uk.gov.ons.sbr.models.reportingunit.{ReportingUnit, Rurn}
 
 object ReadsReportingUnit {
   implicit val rurnReads = Reads.StringReads.map(Rurn(_))
   implicit val ernReads = Reads.StringReads.map(Ern(_))
+  implicit val enterpriseLinkReads = Json.reads[EnterpriseLink]
+  implicit val addressReads = Json.reads[Address]
   implicit val reportingUnitReads = Json.reads[ReportingUnit]
 }
